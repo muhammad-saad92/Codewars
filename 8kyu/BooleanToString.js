fdf8228b1bd661d0f7e0b3de087f8c040fb78f12
muhammad-saad92 <mughalll92@gmail.com>
2025-07-20 01:09:19 +0500

    Added a new file in 8kyu

---
+++ 8kyu/BooleanToString.js
@@ -0,0 +1,6 @@
+// implement a function which convert the given boolean value into its string representation.
+function booleanToString(b){
+    return b === true ? 'true' : 'false';
+  }
+  
+  console.log(booleanToString(true));
