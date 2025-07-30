db266ca018b96f1c6035270d6f966a0106596932
muhammad-saad92 <mughalll92@gmail.com>
2025-07-30 05:19:38 +0500

    Added a remove string spaces challenges

---
+++ 8kyu/removeStringSpaces.js
@@ -0,0 +1,4 @@
+// Write a function that removes the spaces from the string, then return the resultant string.
+const noSpace = x => x.split(" ").join("");
+
+console.log(noSpace('sadfjh askjfh sjha askdhg'))
