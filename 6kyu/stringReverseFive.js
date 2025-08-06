7eead3f45c38932d1e028d32c1586fc0a3e7cf73
muhammad-saad92 <mughalll92@gmail.com>
2025-08-06 06:00:22 +0500

    Changed code in this to work perfectly

--- 6kyu/stringReverseFive.js
+++ 6kyu/stringReverseFive.js
@@ -6,17 +6,23 @@
 // "This is a test        --> "This is a test" 
 // "This is another test" --> "This is rehtona test"
 
+
 function spinWords(string){
+    // This is splitting the string into an array
     let arr = string.split(' ')
-    console.log(arr[2].length)
-    let itemsRet = arr.forEach((item, index) => {
+    let reverseArr = []
+    // Running forEach for all the items in array
+    arr.forEach((item, index) => {
+        // It is checking if any item in array has 5 or more than 5 letters
         if(arr[index].length >= 5){
-            console.log(item.split('').reverse().join(''))
+            // Now we split the array item string into further array, reverse it and joining it again into a string and after it push the item into reverseArr
+            reverseArr.push(item.split('').reverse().join(''))
         } else{
-            console.log(item)
+            // It just pushes the items which are less 5 characters long
+            reverseArr.push(item)
         }
     })
-
+    // After the loop, we join method to join the array into a string
+    return(reverseArr.join(' '))
 }
-
 (spinWords('Hey fellow warriors'))
