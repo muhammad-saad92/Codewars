c3187258fc95badd1309f214117b332d3a012586
muhammad-saad92 <mughalll92@gmail.com>
2025-08-13 07:25:02 +0500

    added a 7 kata

---
+++ 7kyu/leastLarger.js
@@ -0,0 +1,14 @@
+function leastLarger(a,i) {
+    let sorted = a.sort((a,b) => a-b)
+    let max = a[i]
+    let val;
+    for(let index = 0; index < a.length; index++){
+        if(max < sorted[index]){
+            val = sorted[index]
+            break;
+        }
+    }
+    console.log(val)
+  
+  }
+  
