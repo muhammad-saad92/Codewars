8d4d1a278eafa71d95b3f83714027b1edd17b540
muhammad-saad92 <mughalll92@gmail.com>
2025-08-28 19:06:34 +0500

    updated the challenge

--- 6kyu/splitStrings.js
+++ 6kyu/splitStrings.js
@@ -1,16 +1,11 @@
 function solution(str){
-    let arr = [];
-    for(let i = 0; i < str.length; i += 2){
-      arr.push(str.substring(i, i + 2));
-    }
-    
-    let odd = str += '_'
-    
-    
-    if(arr[arr.length - 1].length % 2 !== 0){
-      return odd
-    }
-   
- 
- } 
- console.log(solution('abc'))
+  let arr = str.match(/(..?)/g)
+  let odd;
+  if(arr[arr.length - 1].length % 2 !== 0){
+      odd = arr[arr.length - 1] + '_';
+      arr.pop();
+      arr.push(odd)
+  } 
+  return arr;
+} 
+console.log(solution('abcd'))
