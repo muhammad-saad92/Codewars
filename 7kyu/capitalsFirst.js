d7c574341cdfac18bc328e696d6b00dbf6928ab2
muhammad-saad92 <mughalll92@gmail.com>
2025-09-15 09:04:55 +0500

    fixed a challenge

--- 7kyu/capitalsFirst.js
+++ 7kyu/capitalsFirst.js
@@ -3,14 +3,16 @@ function capitalsFirst(str){
     let newArr = []
     let revArr = []
     arr.forEach(item => {
-        if(item.charAt(0) !== item.charAt(0).toUpperCase()){
+      let code = item.charCodeAt(0)
+        if((code >= 65 && code <= 90) || (code >= 97 && code <= 122)){
+           if(item.charAt(0) !== item.charAt(0).toUpperCase()){
             newArr.push(item);
         } else{
             revArr.unshift(item);
+        } 
         }
     })
           revArr.reverse()
         newArr.unshift(...revArr)
         return newArr.join(' ')
   }
-  console.log(capitalsFirst("hey You, Sort me Already!"))
