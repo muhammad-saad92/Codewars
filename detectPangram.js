855ce7cc1623b029f9dd01ae71b048e4c271631a
muhammad-saad92 <mughalll92@gmail.com>
2025-09-23 17:04:02 +0500

    updated the solution of a challenge

--- detectPangram.js
+++ detectPangram.js
@@ -4,14 +4,13 @@
 
 function isPangram(string){
     let alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    string = string.toLowerCase();
-    for(let i = 0; i <= alphabets.length; i++){
-        if(string.includes(alphabets[i])){
-            return true;
-        } else{
+    string = string.toLowerCase().split('');
+    for(let i = 0; i < alphabets.length; i++){
+        if(!string.includes(alphabets[i])){  
             return false;
-        }
+        } 
     }
+    return true;
 }
 
-console.log(isPangram('This is not a pangram'))
+console.log(isPangram('The quick brown fox jumps over the lazy dog'))
