eacdab63f0ccecddcd5fb8835e12232374c8d5ee
muhammad-saad92 <mughalll92@gmail.com>
2025-09-24 17:29:02 +0500

    added  the solution to the previous challenge

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -10,5 +10,19 @@
 // Note: For 4 or more names, the number in "and 2 others"
 
 function likes(names) {
-    // TODO
+    let arr = [];
+    console.log(arr.length);
+    if(names.length === 0){
+        return('no one likes this');
+    } else if(names.length === 1){
+        return(`${names[0]} likes this`)
+    } else if(names.length === 2){
+        return(`${names[0]} and ${names[1]} like this`)
+    } else if(names.length === 3){
+        return(`${names[0]}, ${names[1]} and ${names[2]} like this`)
+    } else if(names.length >= 4){
+        return(`${names[0]}, ${names[1]} and ${names.length - 2} others like this`)
+    }
   }
+
+console.log(likes([]));
