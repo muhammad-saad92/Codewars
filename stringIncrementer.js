0c8cbf84b5fc04cd0fc44d56e8f5a8cb857c8d3c
muhammad-saad92 <mughalll92@gmail.com>
2025-09-28 00:16:43 +0500

    added the solution to a challenge

--- stringIncrementer.js
+++ stringIncrementer.js
@@ -16,6 +16,51 @@
 
 // Attention: If the number has leading zeros the amount of digits should be considered.
 
-function incrementString (strng) {
-    // return incrementedString
+function incrementString(strng) {
+  let arr = strng.split('');
+  let alphabetArr = [];
+  let numArr = [];
+  let completedArr;
+
+  // Check if last character is a number
+  if (!isNaN(Number(arr[arr.length - 1]))) {
+
+    // Instead of splitting all, we separate only trailing numbers
+    let i = arr.length - 1;
+
+    // Collect digits from the end
+    while (i >= 0 && !isNaN(Number(arr[i]))) {
+      numArr.unshift(arr[i]); // build number from the end
+      i--;
+    }
+
+    // Everything before the number part is alphabets/symbols
+    alphabetArr = arr.slice(0, i + 1);
+
+    // Save the original length of numeric part
+    let originalLength = numArr.length;
+
+    // Increment the numeric part
+    let num = Number(numArr.join('')) + 1;
+
+    // Convert back to string
+    let numStr = String(num);
+
+    // Preserve leading zeros if needed
+    while (numStr.length < originalLength) {
+      numStr = "0" + numStr;
+    }
+
+    // Combine parts
+    completedArr = alphabetArr.join('') + numStr;
+
+  } else {
+    // If string ends with no number, just add "1"
+    completedArr = strng + "1";
   }
+
+  return completedArr;
+}
+
+
+  console.log(incrementString('foo042'))
